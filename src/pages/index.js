7969377f8b0aaeb2b5845e5e9148fc43db848a41
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-09 14:57:22 -0500

    Import sticky component, alter h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import * as React from "react";
-import "../css/custom.css"
-import pageStyles from "../css/custom.css"
-import headingStyles from "../css/custom.css"
+import "../css/custom.css";
+import Sticky from "./sticky";
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Coinsquare Take&nbsp;Home</h1>
-    </main>
+    <div>
+      <h1>Coinsquare Take&nbsp;Home</h1>
+      <Sticky></Sticky>
+    </div>
   );
 };
 
