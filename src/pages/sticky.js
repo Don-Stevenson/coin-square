93a597fcb0e68da35f80fc2076f218bc2e920742
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-09 15:32:54 -0500

    Add classnames to text, button, logo

--- src/pages/sticky.js
+++ src/pages/sticky.js
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../images/logo.png"
 
 function Sticky(props) {
   return (
     <div className="sticky-component">
-      <p>Sticky component</p>
+      <div className="top-line-text">Quick Trade</div>
+      <div className="bottom-line-text">Get it on the App Store</div>
+      <img src={logo} alt="Coinsquare Logo" />
+      <button className="button">Get</button>
     </div>
   );
 }
