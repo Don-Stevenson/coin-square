efb43b125c82859cb7816a9eac928f04912c35ad
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-09 14:58:10 -0500

    Add basic component

---
+++ src/pages/sticky.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Sticky(props) {
+  return (
+    <div className="sticky-component">
+      <p>Sticky component</p>
+    </div>
+  );
+}
+export default Sticky;
